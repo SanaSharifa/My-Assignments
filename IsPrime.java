8b9c01ad6f0116a675354ed5c6f60751aa5e4fb6
Shaik Sana Sharifa <106483892+SanaSharifa@users.noreply.github.com>
2025-01-08 23:30:08 +0530

    Add files via upload

---
+++ IsPrime.java
@@ -0,0 +1,40 @@
+package Week1.Day2;
+
+public class IsPrime {
+	public static void main(String[] args) {
+		int num=13;
+		boolean flag = true;
+		if(num<=1) // 0 and 1 are not prime numbers
+		{
+			flag=false; 
+		}
+		
+		for(int i=2;i<=num;i++)
+		{    
+			//condition for non prime number
+			if(num%i==0)
+			flag=false;
+			break;
+			
+		}
+	
+	if(flag) {
+		System.out.println(num + " is a prime number");
+		
+	}else {
+		System.out.println(num + " is not a prime number");
+		
+	}
+	}
+}
+	
+		
+		
+	
+		
+		
+			
+		
+	
+
+
